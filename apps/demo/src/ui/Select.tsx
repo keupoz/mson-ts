a098fa1d9aeb518bc20b8e8407a030bf679482b1
Ivan Sokolov <keupoz@gmail.com>
2024-10-26 20:48:47 +0700

    Improve select styles

--- apps/demo/src/ui/Select.tsx
+++ apps/demo/src/ui/Select.tsx
@@ -1,13 +1,22 @@
-import { Select as MantineSelect, type SelectProps } from '@mantine/core'
+import { Select as MantineSelect, type SelectProps, Text } from '@mantine/core'
 import { forwardRef } from 'react'
+import classes from './Select.module.scss'
+
+const renderSelectOption: SelectProps['renderOption'] = ({ option }) => (
+  <Text span lineClamp={2} inherit title={option.label}>
+    {option.label}
+  </Text>
+)
 
 export const Select = forwardRef<HTMLInputElement, SelectProps>((props, ref) => {
   return (
     <MantineSelect
       ref={ref}
+      classNames={classes}
       searchable
       nothingFoundMessage="Nothing found"
       placeholder="Pick value"
+      renderOption={renderSelectOption}
       {...props}
     />
   )
