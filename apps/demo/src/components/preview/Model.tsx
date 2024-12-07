9766d076eeab62f75c8ab05ef7e58f72e457fb2a
Ivan Sokolov <keupoz@gmail.com>
2024-12-07 15:14:36 +0700

    Generalize implementation application

--- apps/demo/src/components/preview/Model.tsx
+++ apps/demo/src/components/preview/Model.tsx
@@ -1,3 +1,5 @@
+import type { SlotInfo } from '@keupoz/mson-core'
+import type { Object3D } from 'three'
 import { useEffect, useLayoutEffect } from 'react'
 import { useModel } from '@demo/hooks/useModel'
 import { setAppState } from '@demo/state/appState'
@@ -7,6 +9,24 @@ export interface ModelProps {
   textureUrl: string
 }
 
+const Implementations: Record<string, (slot: SlotInfo<Object3D>) => void> = {
+  'com.minelittlepony.client.model.part.UnicornHorn': (slot) => {
+    const corona = slot.model.getObjectByName('corona')
+
+    if (corona) {
+      corona.visible = true
+      corona.renderOrder = 1
+    }
+  },
+
+  'com.minelittlepony.client.model.part.PonyTail$Segment': (slot) => {
+    const tailStop = slot.info.context.getLocal('segments')
+    const index = slot.info.context.getLocal('segment_index')
+
+    slot.model.visible = index < tailStop
+  },
+}
+
 export function Model({ modelId, textureUrl }: ModelProps) {
   const { model, implementableSlots } = useModel(modelId, textureUrl)
 
@@ -14,14 +34,10 @@ export function Model({ modelId, textureUrl }: ModelProps) {
     // eslint-disable-next-line no-console
     console.log(`Slots of "${modelId}"`, implementableSlots)
 
-    implementableSlots['com.minelittlepony.client.model.part.UnicornHorn']?.forEach((item) => {
-      const corona = item.getObjectByName('corona')
-
-      if (corona) {
-        corona.visible = true
-        corona.renderOrder = 1
-      }
-    })
+    for (const [name, implementation] of Object.entries(Implementations)) {
+      const slots = implementableSlots[name]
+      slots?.forEach(implementation)
+    }
   }, [implementableSlots, modelId])
 
   useEffect(() => {
